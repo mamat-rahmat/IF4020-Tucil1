1670af2c84374b1f7a45fb766253c6f84a7b9e4e
Mamat Rahmat <coderbodoh@gmail.com>
2016-09-02 02:22:08 +0700

    change for style

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -4,20 +4,19 @@
 public class VigenereCipher {
     public static String encrypt(String plaintext, String key) {
         String ciphertext = "";
-        for(int i=0, j=0; i<plaintext.length(); i++){
+        for(int i=0, j=0; i<plaintext.length(); i++, j=(j+1)%key.length()){
             char c = plaintext.charAt(i);
             ciphertext += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
-            j = (j+1) % key.length();
         }
         return ciphertext;
     }
 
     public static String decrypt(String ciphertext, String key) {
         String plaintext = "";
-        for(int i=0, j=0; i<ciphertext.length(); i++){
+        for(int i=0, j=0; i<ciphertext.length(); i++, j=(j+1)%key.length()){
             char c = ciphertext.charAt(i);
             plaintext += (char) ((c - key.charAt(j) + 26) % 26 + 'A');
-            j = (j+1) % key.length();
+
         }
         return plaintext;
     }
