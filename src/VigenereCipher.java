16bce5061b02fd5e42f573b29b6e220035bf1cb5
Mamat Rahmat <coderbodoh@gmail.com>
2016-09-02 02:24:35 +0700

    preprocess input text to UpperCase

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -3,6 +3,9 @@
  */
 public class VigenereCipher {
     public static String encrypt(String plaintext, String key) {
+        plaintext = plaintext.toUpperCase();
+        key = key.toUpperCase();
+
         String ciphertext = "";
         for(int i=0, j=0; i<plaintext.length(); i++, j=(j+1)%key.length()){
             char c = plaintext.charAt(i);
@@ -12,6 +15,9 @@ public class VigenereCipher {
     }
 
     public static String decrypt(String ciphertext, String key) {
+        ciphertext = ciphertext.toUpperCase();
+        key = key.toUpperCase();
+
         String plaintext = "";
         for(int i=0, j=0; i<ciphertext.length(); i++, j=(j+1)%key.length()){
             char c = ciphertext.charAt(i);
